311f6853c1dd1e52ca9510b97654ad37b30c5fa1
jensfoerster <foerster.jens@gmail.com>
2016-04-25 16:34:47 +0200

    added more broadcasts

--- mobile/src/main/java/de/my/playground/fragments/BroadcastFragment.java
+++ mobile/src/main/java/de/my/playground/fragments/BroadcastFragment.java
@@ -24,46 +24,60 @@ public class BroadcastFragment extends Fragment {
 
     private static final String TAG = "BroadcastFragment";
     private BroadcastReceiver mReceiver;
+    private TextView mTextView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        initializeBroadcastListener();
 
         View v = inflater.inflate(R.layout.fragment_broadcast, container, false);
 
+        mTextView = (TextView)v.findViewById(R.id.textview_broadcasts);
+
         FloatingActionButton send = (FloatingActionButton) v.findViewById(R.id.fab_bc);
-        send.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LocalBroadcastManager.getInstance(getActivity()).sendBroadcast(new Intent(getResources().getString(R.string.INTENT_BUTTON_PRESSED)));
-            }
-        });
+        send.setOnClickListener(
+                new Button.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        LocalBroadcastManager.getInstance(getActivity()).sendBroadcast(new Intent(getResources().getString(R.string.INTENT_BUTTON_PRESSED)));
+                    }
+                });
+
+        initializeBroadcastListener();
 
         return v;
     }
 
     private void initializeBroadcastListener() {
-        mReceiver = new BroadcastReceiver(){
+        mReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                TextView tv = (TextView) getActivity().findViewById(R.id.textview_broadcasts);
-                tv.append(intent.getAction() + System.getProperty ("line.separator"));
+                mTextView.append(intent.getAction() + System.getProperty("line.separator"));
             }
         };
 
         getActivity().getApplicationContext().registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_SCREEN_ON));
         getActivity().getApplicationContext().registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_SCREEN_OFF));
+        getActivity().getApplicationContext().registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+        getActivity().getApplicationContext().registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_MEDIA_BUTTON));
+        getActivity().getApplicationContext().registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_POWER_CONNECTED));
+        getActivity().getApplicationContext().registerReceiver(mReceiver, new IntentFilter(Intent.ACTION_POWER_DISCONNECTED));
         LocalBroadcastManager.getInstance(getActivity().getApplicationContext()).registerReceiver(mReceiver, new IntentFilter(getResources().getString(R.string.INTENT_BUTTON_PRESSED)));
     }
 
     @Override
-    public void onDestroyView(){
+    public void onDestroyView() {
         super.onDestroyView();
         /* this happens when the receiver has been unregistered or not yet registered */
-        try{ getActivity().getApplicationContext().unregisterReceiver(mReceiver); }
-        catch (IllegalArgumentException iae) { Log.e(TAG, iae.getMessage()); }
-        try{ LocalBroadcastManager.getInstance(getActivity().getApplicationContext()).unregisterReceiver(mReceiver); }
-        catch (IllegalArgumentException iae) { Log.e(TAG, iae.getMessage()); }
+        try {
+            getActivity().getApplicationContext().unregisterReceiver(mReceiver);
+        } catch (IllegalArgumentException iae) {
+            Log.e(TAG, iae.getMessage());
+        }
+        try {
+            LocalBroadcastManager.getInstance(getActivity().getApplicationContext()).unregisterReceiver(mReceiver);
+        } catch (IllegalArgumentException iae) {
+            Log.e(TAG, iae.getMessage());
+        }
     }
 }
